80cb27b0a85abb1f4a2ea47a49ffb1c4449d6658
mediatekvpn <115091683+mediatekvpn@users.noreply.github.com>
2022-10-23 21:46:38 +0800

    Update auth_prem.sh

--- auth_prem.sh
+++ auth_prem.sh
@@ -3,9 +3,9 @@ error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
 $DB_host = 'mysql2.blazingfast.io';
-$DB_user = 'mediatek2_sshmediatek';
-$DB_pass = 'mediatekssh';
-$DB_name = 'mediatek2_sshmediatek';
+$DB_user = 'mediatek2_test';
+$DB_pass = 'F1005r90FF';
+$DB_name = 'mediatek2_test';
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
